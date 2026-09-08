9a5a7676e284ed639132bbcabf76c7a49c74f61d
muhamedovzamsid2-lang <muhamedovzamsid2@gmail.com>
2026-09-08 18:50:15 +0500

    Run legacy server behind V4 gateway

--- ULGURJI_ONLINE_NAZORAT/boot.js
+++ ULGURJI_ONLINE_NAZORAT/boot.js
@@ -1,2 +1,8 @@
 await import('./import-employees.js');
+const publicPort=Number(process.env.PORT||3000);
+process.env.INTERNAL_PORT='3001';
+process.env.PORT='3001';
 await import('./server.js');
+process.env.PUBLIC_PORT=String(publicPort);
+process.env.PORT=String(publicPort);
+await import('./gateway.js');

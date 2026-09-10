f7c69e98f791759eb9ada3a0a6d751acec01f0a0
muhamedovzamsid2-lang <muhamedovzamsid2@gmail.com>
2026-09-10 18:08:41 +0500

    Route employee panel APIs to hardened v2 bridge

--- ULGURJI_ONLINE_NAZORAT/employee-entry-bridge.js
+++ ULGURJI_ONLINE_NAZORAT/employee-entry-bridge.js
@@ -1,18 +1,5 @@
 import http from 'node:http';
-import {handleEmployeeApi} from './employee-entry-api.js';
-
+import {handleEmployeeApi as handleEmployeeV2} from './employee-entry-v2.js';
+import {handleEmployeeApi as handleEmployeeLegacy} from './employee-entry-api.js';
 const originalCreateServer=http.createServer;
-if(!http.__ULGURJI_EMPLOYEE_ENTRY_BRIDGE){
-  http.createServer=function(listener,...args){
-    const wrapped=async(req,res)=>{
-      const pathname=new URL(req.url,'http://local').pathname;
-      if(pathname.startsWith('/api/employee-')||pathname.startsWith('/api/manager-employee-')){
-        const handled=await handleEmployeeApi(req,res,pathname);
-        if(handled)return;
-      }
-      return listener(req,res);
-    };
-    return originalCreateServer.call(http,wrapped,...args);
-  };
-  Object.defineProperty(http,'__ULGURJI_EMPLOYEE_ENTRY_BRIDGE',{value:true});
-}
+if(!http.__ULGURJI_EMPLOYEE_ENTRY_BRIDGE){http.createServer=function(listener,...args){const wrapped=async(req,res)=>{const pathname=new URL(req.url,'http://local').pathname;let handled=false;if(pathname.startsWith('/api/employee-'))handled=await handleEmployeeV2(req,res,pathname);else if(pathname.startsWith('/api/manager-employee-'))handled=await handleEmployeeLegacy(req,res,pathname);if(handled)return;return listener(req,res)};return originalCreateServer.call(http,wrapped,...args)};Object.defineProperty(http,'__ULGURJI_EMPLOYEE_ENTRY_BRIDGE',{value:true})}
